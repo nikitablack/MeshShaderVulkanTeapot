c5740837b12eed1c601145e14979897148a2628c
nc <nikita@nodarsensor.com>
2026-01-24 19:44:42 +0100

    Removed unnecessary CONCAT

--- libs/utils/include/utils/try_expected.hpp
+++ libs/utils/include/utils/try_expected.hpp
@@ -1,11 +1,9 @@
 #include <expected>
 
-#define CONCAT(a, b) a##b
-
-#define TRY_EXPECTED_VOID_IMPL(expr, uniq)                   \
-    auto CONCAT(_tmp_, uniq) = expr;                         \
-    if (!CONCAT(_tmp_, uniq).has_value()) {                  \
-        return std::unexpected{CONCAT(_tmp_, uniq).error()}; \
+#define TRY_EXPECTED_VOID(expr)                    \
+    {                                              \
+        auto _tmp_{expr};                          \
+        if (!_tmp_.has_value()) {                  \
+            return std::unexpected{_tmp_.error()}; \
+        }                                          \
     }
-
-#define TRY_EXPECTED_VOID(expr) TRY_EXPECTED_VOID_IMPL(expr, __COUNTER__)
